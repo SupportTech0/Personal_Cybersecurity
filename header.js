e0aba165af9f75bf2726d202c1b1aac8e4ea5290
SupportTech0 <0011.tech.s.0011@gmail.com>
2025-01-14 14:52:39 +0100

    Update header.js

--- header.js
+++ header.js
@@ -5,7 +5,7 @@ const headContent = `
     <link rel="icon" href="favicon.ico" type="image/x-icon">
     <!-- Security Headers -->
     <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src 'self' https:;">
-    <meta http-equiv="X-Frame-Options" content="DENY">
+    <meta http-equiv="X-Frame-Options" content="SAMEORIGIN">
     <meta http-equiv="Strict-Transport-Security" content="max-age=31536000; includeSubDomains; preload">
     <meta http-equiv="X-Content-Type-Options" content="nosniff">
     <meta http-equiv="Referrer-Policy" content="no-referrer">
